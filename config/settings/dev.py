ea005d8cc636224a275386d9881e1bb1d79ae114
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-08-26 20:57:50 +0500

    fix: ci

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -14,7 +14,7 @@ if TEST:
     CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": "redis://redis:6379/1",
+            "LOCATION": "redis://redis:6379/0",
             "OPTIONS": {
                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
             }
