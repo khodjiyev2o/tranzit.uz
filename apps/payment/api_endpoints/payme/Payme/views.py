7d0b42df0fb0c5ba5b2695025ba422781cdd8d71
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-09-14 20:46:27 +0500

    feaT: least amount is 10 000som

--- apps/payment/api_endpoints/payme/Payme/views.py
+++ apps/payment/api_endpoints/payme/Payme/views.py
@@ -63,7 +63,7 @@ class PaymeAPIView(PaymentView):
             return dict(error=dict(code=code, message=error_message))
 
         transaction = (
-            Transaction.objects.filter(order_id=self.params["account"]["order_id"], transaction_id=self.params["id"])
+            Transaction.objects.filter(order__driver__user__phone=self.params["account"]["phone"], transaction_id=self.params["id"])
             .order_by("-id")
             .first()
         )
